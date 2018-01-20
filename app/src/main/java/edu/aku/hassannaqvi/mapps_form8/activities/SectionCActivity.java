746d6bcd045bdc880ca32b15c6cddb38afc22eaa
gul.sanober <gulsanobermemon@gmail.com>
2018-01-20 14:17:34 +0500

    Form 8 activities added

---
+++ app/src/main/java/edu/aku/hassannaqvi/mapps_form8/activities/SectionCActivity.java
@@ -0,0 +1,15 @@
+package edu.aku.hassannaqvi.mapps_form8.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.mappsform8.R;
+
+public class SectionCActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_c);
+    }
+}
