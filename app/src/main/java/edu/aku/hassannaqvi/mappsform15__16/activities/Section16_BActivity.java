241e3371968f5ce85430f4a9b92b77d491a9189b
mustafa.ansari <mustufa_ayub@hotmail.com>
2018-08-09 16:27:27 +0500

    app logo updated and minor changes in Sections

--- app/src/main/java/edu/aku/hassannaqvi/mappsform15__16/activities/Section16_BActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/mappsform15__16/activities/Section16_BActivity.java
@@ -31,6 +31,8 @@ public class Section16_BActivity extends AppCompatActivity {
 
         bi.mp16b02.setManager(getSupportFragmentManager());
         bi.mp16b02.setMaxDate(new SimpleDateFormat("dd/MM/yyyy").format(System.currentTimeMillis()));
+        String date = "30/06/2017";
+        bi.mp16b02.setMinDate(date);
 
     }
 
@@ -83,7 +85,7 @@ public class Section16_BActivity extends AppCompatActivity {
 
         sB.put("mp16b01", bi.mp16b02.getText().toString());
         sB.put("mp16b02", bi.mp16b01a.isChecked() ? "1" : bi.mp16b01b.isChecked() ? "2" : bi.mp16b01c.isChecked() ? "3" : bi.mp16b01d.isChecked() ? "4" : bi.mp16b01e.isChecked() ? "5" : bi.mp16b01f.isChecked() ? "6" : bi.mp16b01g.isChecked() ? "7" : bi.mp16b01h.isChecked() ? "8" : bi.mp16b01i.isChecked() ? "88" : "0");
-        sB.put("mp16b02", bi.mp16b01ix.getText().toString());
+        sB.put("mp16b02x", bi.mp16b01ix.getText().toString());
         sB.put("mp16b03", bi.mp16b03.getText().toString());
         AppMain.fc.setsB(String.valueOf(sB));
     }
