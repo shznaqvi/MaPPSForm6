9deb3324e623f22978b1b29fac4a5d95bf5c9f99
mustafa.ansari <mustufa_ayub@hotmail.com>
2018-08-31 15:38:24 +0500

    minor changes in form 15 and 16

--- app/src/main/java/edu/aku/hassannaqvi/mappsform15__16/activities/Section16_BActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/mappsform15__16/activities/Section16_BActivity.java
@@ -84,8 +84,8 @@ public class Section16_BActivity extends AppCompatActivity {
         JSONObject sB = new JSONObject();
 
         sB.put("mp16b01", bi.mp16b02.getText().toString());
-        sB.put("mp16b02", bi.mp16b01a.isChecked() ? "1" : bi.mp16b01b.isChecked() ? "2" : bi.mp16b01c.isChecked() ? "3" : bi.mp16b01d.isChecked() ? "4" : bi.mp16b01e.isChecked() ? "5" : bi.mp16b01f.isChecked() ? "6" : bi.mp16b01g.isChecked() ? "7" : bi.mp16b01h.isChecked() ? "8" : bi.mp16b01i.isChecked() ? "88" : "0");
-        sB.put("mp16b02x", bi.mp16b01ix.getText().toString());
+        sB.put("mp16b02", bi.mp16b01a.isChecked() ? "1" : bi.mp16b01b.isChecked() ? "2" : bi.mp16b01c.isChecked() ? "3" : bi.mp16b01d.isChecked() ? "4" : bi.mp16b01e.isChecked() ? "5" : bi.mp16b01f.isChecked() ? "6" : bi.mp16b01g.isChecked() ? "7" : bi.mp16b01h.isChecked() ? "8" : bi.mp16b01i.isChecked() ? "9" : bi.mp16b01j.isChecked() ? "88" :"0");
+        sB.put("mp16b02x", bi.mp16b01jx.getText().toString());
         sB.put("mp16b03", bi.mp16b03.getText().toString());
         AppMain.fc.setsB(String.valueOf(sB));
     }
@@ -111,15 +111,15 @@ public class Section16_BActivity extends AppCompatActivity {
             bi.mp16b01.clearFocus();
         }
 
-        if (bi.mp16b01i.isChecked()) {
+        if (bi.mp16b01j.isChecked()) {
 
-            if (!validatorClass.EmptyTextBox(this, bi.mp16b01ix, getString(R.string.mp16b01))) {
+            if (!validatorClass.EmptyTextBox(this, bi.mp16b01jx, getString(R.string.mp16b01))) {
 
-                bi.mp16b01ix.requestFocus();
+                bi.mp16b01jx.requestFocus();
                 return false;
             } else {
 
-                bi.mp16b01ix.clearFocus();
+                bi.mp16b01jx.clearFocus();
             }
         }
 
